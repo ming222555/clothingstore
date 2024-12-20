dc46e69f06e213f2c7562de26fbdc3fbce258567
ming <ming222555@yahoo.com>
2024-12-20 19:07:35 +0800

    "node_modules/bootstrap/scss/utilities/api" is needed for "bootstrap/scss/utilities". Basic verify Bootstrap flex, grid, and utilities.

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -2,6 +2,34 @@ export default function Home() {
   return (
     <div>
       Home page
+      <div>
+        <p>Basic verify Bootstrap flex, grid, and utilities</p>
+        <br />
+        <div className="container text-center text-primary">
+          <div
+            className="row align-items-start"
+            style={{ background: "lightgreen" }}
+          >
+            <div
+              className="col-3 order-last"
+              style={{ outline: "1px solid blue" }}
+            >
+              1 in Dom, col-3 order-last
+            </div>
+            <div className="col-md-5" style={{ outline: "1px solid blue" }}>
+              col-md-5
+            </div>
+            <div
+              className="col align-self-center"
+              style={{ outline: "1px solid blue" }}
+            >
+              <p className="bg-info p-2">col</p>
+            </div>
+            <span className="d-none">span</span>
+          </div>
+        </div>
+      </div>
+      <br />
       <button className="btn btn-info">Bootstrap button</button>
     </div>
   );
