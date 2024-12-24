62c56713044fabf94823e27590218565801b5e2b
ming <ming222555@yahoo.com>
2024-12-24 13:34:40 +0800

    Begin working on CartSummaryNav

--- src/ui/main-nav/cart-summary-nav.tsx
+++ src/ui/main-nav/cart-summary-nav.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 import { ShoppingBagIcon } from "lucide-react";
 
 const CartFallback = () => (
-  <div style={{ opacity: 0.3 }}>
+  <div className="ms-3 opacity-25">
     <ShoppingBagIcon />
   </div>
 );
@@ -16,6 +16,12 @@ export default function CartSummaryNav() {
   );
 }
 
+const CartSummaryNavInnerContent = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => <>{children}</>;
+
 const CartSummaryNavInner = async () => {
   const cookieValues = await cookies();
   cookieValues.get("yns_cart");
@@ -24,17 +30,22 @@ const CartSummaryNavInner = async () => {
     setTimeout(resolv, 6666);
   });
 
+  // CartSummaryNavInnerContent is a server component.
+  // In below, we will be wrapping <CartSummaryNavInnerContent> with
+  // client component that is a context provider.
   return (
-    <>
-      <p>totalItems 123</p>
-      <ShoppingBagIcon />
-      {/* 1. Await if cookie ysn_cart exists
+    <CartSummaryNavInnerContent>
+      <>
+        <p>totalItems 123</p>
+        <ShoppingBagIcon />
+        {/* 1. Await if cookie ysn_cart exists
       2. If no exist, return <CartFallback />
       3. Otherwise, get cart totalItems and total amount.
       4. Display <ShoppingBagIcon /> and totalItems.
       5. Tooltip hovers to show totalItems and total amount.
         (react bootstrap popover>)
        */}
-    </>
+      </>
+    </CartSummaryNavInnerContent>
   );
 };
