39626cf32836bfd0edc2f1d47a4fa0c1188cd51d
ming <ming222555@yahoo.com>
2024-12-29 13:55:47 +0800

    CartModal   cart lines: loop thru array of hard coded dummy items

--- src/ui/cart-modal/cart-modal.tsx
+++ src/ui/cart-modal/cart-modal.tsx
@@ -4,6 +4,49 @@ import CartModalBackdrop from "./cart-modal-backdrop";
 import YnsLink from "@/ui/yns-link";
 
 export default function CartModal() {
+  const cart = {
+    currency: "USD",
+    total: 750350.94,
+    lines: [
+      {
+        product: {
+          id: "watch",
+          name: "Watch",
+          imgSrc: "/prod123.jpg",
+          price: 249999,
+          qty: 3,
+        },
+      },
+      {
+        product: {
+          id: "single-sock",
+          name: "Single sock",
+          imgSrc: "/prod123.jpg",
+          price: 4.99,
+          qty: 6,
+        },
+      },
+      {
+        product: {
+          id: "one-shoe",
+          name: "One shoe",
+          imgSrc: "/prod123.jpg",
+          price: 299,
+          qty: 1,
+        },
+      },
+      {
+        product: {
+          id: "sunbeam-tote-ray-tomasz",
+          name: "Sunbeam Tote Ray Tomasz",
+          imgSrc: "/prod123.jpg",
+          price: 25,
+          qty: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <>
       <CartModalBackdrop />
@@ -18,7 +61,37 @@ export default function CartModal() {
           </YnsLink>
         </header>
         <ul className="flex-grow-1 p-3 pt-0 m-0 overflow-auto">
-          <li className="py-4 border-bottom border-secondary-subtle">
+          {cart.lines.map((line) => (
+            <li
+              className="py-4 border-bottom border-secondary-subtle"
+              key={line.product.id}
+            >
+              <div className="d-flex">
+                <div
+                  className="position-relative bg-warning align-self-center"
+                  style={{
+                    width: "3.8rem",
+                    height: "3.8rem",
+                    outline: "1px solid blue",
+                  }}
+                >
+                  <Image src={line.product.imgSrc} fill alt="" />
+                </div>
+                <div className="flex-grow-1 d-flex flex-column bg-warning mx-3">
+                  <p className="CartModal__product-name p-0 m-0 flex-grow-1 fw-semibold">
+                    {line.product.name}
+                  </p>
+                  <p className="fs-0875 fw-medium p-0 mt-2 mb-0 bg-info text-black-50">
+                    Quantity: {line.product.qty}
+                  </p>
+                </div>
+                <div className="fs-0875 fw-medium bg-warning">
+                  <p>${line.product.price}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+          {/* <li className="py-4 border-bottom border-secondary-subtle">
             <div className="d-flex">
               <div
                 className="position-relative bg-warning align-self-center"
@@ -67,7 +140,7 @@ export default function CartModal() {
                 <p>$299.00</p>
               </div>
             </div>
-          </li>
+          </li> */}
           <li>line 3</li>
           <li>line 1</li>
           <li>line 2</li>
