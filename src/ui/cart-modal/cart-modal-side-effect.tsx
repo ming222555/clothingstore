2584fc8b8a16830919a6b0f36f9c79d8c1f47c76
ming <ming222555@yahoo.com>
2025-01-06 20:38:34 +0800

    New approach cartModalProvider-method will be created and pursued. See the comments at CartModalSideEffect for reason behind it.

--- src/ui/cart-modal/cart-modal-side-effect.tsx
+++ src/ui/cart-modal/cart-modal-side-effect.tsx
@@ -18,6 +18,33 @@ export default function CartModalSideEffect({
     }
 
     startTransition(async () => {
+      /**
+       * We will abandon below addToCartApi() call method.
+       * This is cos if user clicks on modal backdrop to close modal,
+       * thereby unmounting CartModalSideEffect, then subsequent router.refresh()
+       * below will not have chance to run, which would mean layout (and thus CartSummary)
+       * will not rerender.
+       *
+       * A solution would be to use "await addToCartAction(add)" server action method, as
+       * in "/home/ming/coding/yournextstore => CartModalAddSideEffect".
+       * This method rerenders layout (and thus CartSummary) even if user has closed modal,
+       * since a cookie.set() call in server action will automatically refresh current
+       * route (indeed, a cookie.set() call in server action will clear client router cache),
+       * regardless whether user is still on cart modal page or has left it.
+       * But if still on cart modal page, the page will rerender with searchparam add still around.
+       * This would optimistically however repeat add cart product once again, which is incorrect display,
+       * albeit that a subsequent manual "router.replace("/cart-overlay"); router.refresh()" below
+       * will refresh cart modal page without searchparam add this time around and display correct cart content.
+       *
+       * Another solution is shown at "/home/ming/coding/yournextstorev2-no-use-@modal" by using
+       * a CartModalProvider method.
+       * It is this method then that we shall purse for
+       * /home/ming/coding/yournextjsstore
+       * In view of this, a new branch (cartModalProvider-method) will be created in this git repo
+       * for such CartModalProvider method prior to porting over to
+       * /home/ming/coding/yournextjsstore
+       *
+       */
       await addToCartApi(add);
       // const data = await addToCart(add);
       // const error = data.error;
