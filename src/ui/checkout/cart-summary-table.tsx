a67c1dbe5f40916a48556600319ee144173d757d
ming <ming222555@yahoo.com>
2025-01-24 13:28:36 +0800

    Basic styling of CartSummaryTable for CartLayout

--- src/ui/checkout/cart-summary-table.tsx
+++ src/ui/checkout/cart-summary-table.tsx
@@ -6,10 +6,13 @@ import YnsLink from "@/ui/yns-link";
 export default async function CartSummaryTable({ cart }: { cart: Cart }) {
   console.log("CartSummaryTable cart", cart);
   return (
-    <table className="table table-bordered">
+    <table className="CartSummaryTable table table-bordered table-sm align-middle">
       <thead>
         <tr>
-          <th scope="col">
+          <th
+            scope="col"
+            className="CartSummaryTable__th-image d-none d-sm-table-cell"
+          >
             <span className="visually-hidden">Image</span>
           </th>
           <th scope="col">Product</th>
@@ -22,17 +25,23 @@ export default async function CartSummaryTable({ cart }: { cart: Cart }) {
       </thead>
       <tbody>
         <tr>
-          <th scope="row">
-            <Image src="/popo.jpg" width={80} height={80} alt="" />
-          </th>
+          <td className="CartSummaryTable__td-image d-none d-sm-table-cell">
+            <Image
+              src="/popo.jpg"
+              width={80}
+              height={80}
+              alt=""
+              className="d-block"
+            />
+          </td>
           <td>
             <YnsLink href="/product/horizon-gaze-sunglasses">
               Horizon Gaze Sunglasses
             </YnsLink>
           </td>
           <td>$20.00</td>
-          <td className="text-end">
-            <span>
+          <td>
+            <span className="d-flex flex-column flex-sm-row align-items-center justify-content-between p-1">
               <button>
                 <span>-</span>
               </button>
@@ -42,12 +51,20 @@ export default async function CartSummaryTable({ cart }: { cart: Cart }) {
               </button>
             </span>
           </td>
-          <td className="text-end">$20.00</td>
+          <td className="text-end" width={16}>
+            $201.00
+          </td>
         </tr>
         <tr>
-          <th scope="row">
-            <Image src="/popo.jpg" width={80} height={80} alt="" />
-          </th>
+          <td className="CartSummaryTable__td-image d-none d-sm-table-cell">
+            <Image
+              src="/popo.jpg"
+              width={80}
+              height={80}
+              alt=""
+              className="d-block"
+            />
+          </td>
           <td>
             <YnsLink href="/product/sunbeam-tote-jeff">
               Sunbeam Tote Jeff
@@ -55,8 +72,8 @@ export default async function CartSummaryTable({ cart }: { cart: Cart }) {
           </td>
 
           <td>$25.00</td>
-          <td className="text-end">
-            <span>
+          <td>
+            <span className="d-flex flex-column flex-sm-row align-items-center justify-content-between p-1">
               <button>
                 <span>-</span>
               </button>
@@ -67,11 +84,12 @@ export default async function CartSummaryTable({ cart }: { cart: Cart }) {
             </span>
           </td>
           <td className="text-end">
-            <span className="text-nowrap">$6625.00</span>
+            <span className="text-break">$662588888888888888.00</span>{" "}
+            {/* text-nowrap */}
           </td>
         </tr>
         <tr>
-          <th scope="row"></th>
+          <td className="CartSummaryTable__td-image d-none d-sm-table-cell"></td>
           <td colSpan={3} className="text-end pe-3">
             TOTAL
           </td>
