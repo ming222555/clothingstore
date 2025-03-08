7877415acc80392f4e0037c24cf1eb595c82635d
ming <ming222555@yahoo.com>
2025-03-08 22:40:49 +0800

    Add padding to CheckoutSuccessPage for mobile web browser sake

--- src/app/(store)/checkout-success/page.tsx
+++ src/app/(store)/checkout-success/page.tsx
@@ -11,7 +11,7 @@ export default async function CheckoutSuccessPage(props: {
 
   return (
     <>
-      <div className="position-fixed top-0 start-0 bottom-0 end-0 bg-white z-1 d-flex align-items-center justify-content-center flex-column">
+      <div className="position-fixed top-0 start-0 bottom-0 end-0 bg-white z-1 d-flex align-items-center justify-content-center flex-column px-5">
         <h2 className="h2">Checkout successful</h2>
         <br />
         <br />
