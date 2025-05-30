4ec64579e2529df813572f61e1da46d88d2bbd92
ming <ming222555@yahoo.com>
2025-05-31 00:50:06 +0800

    bs -> tw AllProducts

--- src/ui/products/products.tsx
+++ src/ui/products/products.tsx
@@ -6,22 +6,22 @@ import { formatMoney } from "@/lib/utils/utils";
 
 export default function AllProducts({ products }: { products: Product[] }) {
   return (
-    <div className="AllProducts mb-5">
-      <h1 className="h2 fw-semibold mt-5 mb-4">All Products</h1>
+    <div className="mb-20 mt-[-1rem]">
+      <h1 className="h2 font-bold mt-5 mb-4">All Products</h1>
       <div
-        className={`row g-0 row-gap-3 row-cols-1 row-cols-sm-2 row-cols-xl-3 bg-body-tertiary${
-          products.length ? " d-flex" : " d-none"
+        className={`grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 ${
+          products.length ? "flex" : "hidden"
         }`}
       >
         {products.length &&
           products.map((p, idx) => (
-            <div className="col" key={p.id}>
+            <div key={p.id}>
               <YnsLink
                 href={`/product/${p.id}`}
-                className="AllProducts__details shadow-sm d-block"
+                className="group shadow-sm block"
               >
                 <Image
-                  className="AllProducts__image w-100 h-auto product-image"
+                  className="group-hover:opacity-75 rounded-xl"
                   src={p.img_src}
                   width="736"
                   height="736"
@@ -30,13 +30,13 @@ export default function AllProducts({ products }: { products: Product[] }) {
                   alt=""
                 />
                 <div className="ps-2">
-                  <h3 className="h5 fw-semibold">
-                    <span className="d-inline-block pt-3 text-dark">
+                  <h3 className="h5 font-bold">
+                    <span className="inline-block pt-3 text-black">
                       {p.name}
                     </span>
                   </h3>
                   <div className="pb-3">
-                    <span className="h6 text-body-tertiary">
+                    <span className="h6 text-gray-500">
                       {formatMoney(p.unit_price, p.currency)}
                     </span>
                   </div>
